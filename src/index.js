8b5d423348e264c81e47751d1ec0d172f9967f2a
Caleb <calebcoliver@gmail.com>
2024-05-12 12:22:04 -0400

    Word List

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //----------------------------------
 // Fetch Word
 //----------------------------------
-/*
-const randWordURL = 'http://www.randomlists.com/data/words.json'
+
+const randWordURL = '.src/words.json'
 
 async function randWordFind() {
   return fetch(randWordURL).then(res => res.json());
@@ -10,7 +10,7 @@ async function randWordFind() {
 
 let randomWord = await randWordFind()
 
-const dictionaryURL = 'http://api.dictionaryapi.dev/api/v2/entries/en/' + randomWord
+const dictionaryURL = 'http://api.dictionaryapi.dev/api/v2/entries/en/' + randomWord[1]
 
 async function dictFind() {
     return fetch(jsonDataURL).then(res => res.json());
@@ -19,26 +19,3 @@ async function dictFind() {
 let randomWordDict  = await dictFind();
 
 console.log(randomWord)
-*/
-// index.js
-import express from "express";
-
-// Define "require"
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-
-const data = require("./data.json");
-
-const app = express();
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.status(200).send(data);
-});
-
-app.listen(3000, (error) => {
-  if (error) {
-    throw new Error(error);
-  }
-
-  console.log("Backend is running");
-});
