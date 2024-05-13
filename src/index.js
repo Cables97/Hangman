fdfb3fa93e86509b540a1e09c570adf79fd8daa1
Caleb <calebcoliver@gmail.com>
2024-05-13 15:16:28 -0400

    Buttons and Blanks
    Need to build hud, win state and menus. Reveal timer as well.
    
    Buttons function. Blanks have cool animation, placeholder background

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { buildKeyboard, buildBlankBox} from "./modules/elementBuilder.js";
+
+
 //----------------------------------
 // Fetch Word
 //----------------------------------
@@ -25,17 +28,101 @@ return randomWordDict
 }
 
 let word = await randomWordGen();
-console.log(word[0])
+const arrayWord = word[0]["word"].split("");
+//----------------------------------
+// Keyboard Init
+//----------------------------------
 
- 
+let chosenLetters = [];
+buildKeyboard();
 
-document.getElementById('btn').addEventListener("click", () =>{
+document.querySelectorAll(".key").forEach(element => {
+    element.addEventListener("click", () =>{
+        toggleKeyPick(element)
+    })
+});
 
-    if(document.getElementById('btn').classList.contains('active')){
-        document.getElementById('btn').classList.remove('active')
-        
-    } else{
-        document.getElementById('btn').classList.add('active')
+function toggleKeyPick(el){
+    el.preventDefault;
+    el.classList.add('click')
+        setTimeout(() => { el.classList.remove('click') }, 200);
+    el.offsetWidth;
+
+    if(el.classList.contains("down")){
+        console.log("already picked")
+    }else{
+        el.classList.add('down')
+        letterCheck(el.dataset.label)
     }
+}
+
+
+//----------------------------------
+// Blanks Init
+//----------------------------------
+
+initializeBlanks();
+
+function initializeBlanks(){
+
+    buildBlankBox(word);
+    let blanks = document.querySelectorAll(".blank");
+    blanks.forEach(element => {
+        element.addEventListener("click", () => {
+        let letter = element.getElementsByClassName("letter")[0].innerHTML
+        })
+    });
+    for(let i = 0; i < blanks.length; i++){
+        blanks[i].classList.add("show")
+    }
+ 
+}
+
+
+//----------------------------------
+//  Letter Checking
+//----------------------------------
+
+function letterCheck(letter){
+    console.log(letter)
+    chosenLetters.push(letter)
+    if(arrayWord.includes(letter)){
+        console.log("found one")
+        letterReveal(letter)
+    }
+}
+
+function letterReveal(letter){
+    let blanks = document.querySelectorAll(".blank");
     
-})
+    blanks.forEach(blank => {
+        
+        let lett = blank.getElementsByClassName("letter")[0].innerHTML
+        if(lett === letter){
+            setTimeout(function() {
+                console.log("banana " + letter)
+            }, 1000);
+        }
+    })
+}
+
+function winCheck(){
+}
+
+let timer;
+let domTimer = document.getElementById("timer");
+
+//timerInit();
+
+function timerInit(){
+    timer = 0;
+    setInterval(timerInc, 1000);
+
+}
+
+function timerInc(){
+    timer++;
+    domTimer.innerHTML = timer
+    //console.log(timer)
+
+}
---
+++ src/modules/elementBuilder.js
@@ -0,0 +1,61 @@
+//----------------------------------
+// Build Keyboard
+//----------------------------------
+
+function buildKeyboard(){
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const arrayAlphabet = alphabet.split("");
+    const domKeyboard = document.getElementById("keyboard");
+
+    for(let i = 0; i < arrayAlphabet.length ; i++){
+        let letter = arrayAlphabet[i];
+
+        let button = document.createElement('div');
+        button.classList.add(letter);
+        button.classList.add("btn");
+        button.classList.add("key");
+        button.dataset.label = letter;
+        domKeyboard.append(button);
+    }
+}
+
+//----------------------------------
+// Build Game Board
+//----------------------------------
+
+function buildBlankBox(word){
+    console.log(word[0])
+    const arrayWord = word[0]["word"].split("");
+    arrayWord.forEach(letter => {
+        buildBlank(letter)
+    });  
+}
+
+function buildBlank(letter){
+    const domBlanksBox = document.getElementById("blank-wrapper");
+
+    let blank = document.createElement('div');
+        blank.classList.add("blank");
+        domBlanksBox.append(blank);
+
+    let left = document.createElement('div');
+        left.classList.add("b-s");
+        left.classList.add("left");
+        blank.append(left);
+
+    let right = document.createElement('div');
+        right.classList.add("b-s");
+        right.classList.add("right");
+        blank.append(right);
+
+    let text = document.createElement('div');
+        text.classList.add("letter");
+        text.innerHTML = letter;
+        blank.append(text);
+}
+
+
+
+
+
+export { buildKeyboard, buildBlankBox }
